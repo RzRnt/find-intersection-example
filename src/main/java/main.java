97dce606956089e6bde57fcf46a80cfbe2a9073f
Rezi Rusnadiputra <rezi.rusnadiputra@bukalapak.com>
2019-12-21 18:44:50 +0700

    Init Project - Crasfeintsan

---
+++ src/main/java/main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.*;
+
+
+class main
+{
+    public static void main ( String [] arguments )
+    {
+        ArrayList<String> x = new ArrayList(Arrays.asList("abc", "WALALA"));
+        ArrayList<String> dua = new ArrayList<String>();
+
+        System.out.println(getCommonWords(x, dua));
+    }
+
+    public static ArrayList<String> getCommonWords(ArrayList<String> list1, ArrayList<String> list2) {
+        ArrayList<String> tiga = new ArrayList<String>();
+        return list1;
+
+    }
+}
