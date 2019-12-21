0f395f3a1c1a1bd947e6739e04656492bfe06731
Rezi Rusnadiputra <rezi.rusnadiputra@bukalapak.com>
2019-12-21 19:05:32 +0700

    Get Common Words - Brute Force

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,15 +6,19 @@ class main
 {
     public static void main ( String [] arguments )
     {
-        ArrayList<String> x = new ArrayList(Arrays.asList("abc", "WALALA"));
-        ArrayList<String> dua = new ArrayList<String>();
+        ArrayList<String> x = new ArrayList(Arrays.asList("find", "intersection", "two", "lists"));
+        ArrayList<String> dua = new ArrayList<>(Arrays.asList("find", "between", "two", "lists"));
 
         System.out.println(getCommonWords(x, dua));
     }
 
     public static ArrayList<String> getCommonWords(ArrayList<String> list1, ArrayList<String> list2) {
-        ArrayList<String> tiga = new ArrayList<String>();
-        return list1;
-
+        ArrayList<String> commonWords = new ArrayList<>();
+        list1.forEach( list -> {
+            if (list2.contains(list)) {
+                commonWords.add(list);
+            }
+        });
+        return commonWords;
     }
 }
