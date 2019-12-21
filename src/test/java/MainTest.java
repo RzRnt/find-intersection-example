e2063478fcbad306b77b25e84477a678f85d67a5
Rezi Rusnadiputra <rezi.rusnadiputra@bukalapak.com>
2019-12-21 21:58:57 +0700

    Add Unit Test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MainTest {
+
+    @Test
+    public void ShouldReturnCommonWords() {
+        ArrayList<String> firstWords = new ArrayList(Arrays.asList("find", "intersection", "two", "lists"));
+        ArrayList<String> secondWords = new ArrayList<>(Arrays.asList("find", "between", "two", "lists"));
+        ArrayList<String> expectedCommonWords = new ArrayList<>(Arrays.asList("find", "two", "lists"));
+        Main m = new Main();
+        ArrayList<String> actualCommonWords = m.getCommonWords(firstWords, secondWords);
+        Assert.assertEquals(expectedCommonWords, actualCommonWords);
+    }
 }
+
