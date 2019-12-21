db426f2f4e0b6d1ff92281e7db26cabbf0d799ee
Rezi Rusnadiputra <rezi.rusnadiputra@bukalapak.com>
2019-12-22 00:17:33 +0700

    Set Iro-Iro

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,49 @@
+import com.github.javafaker.Faker;
 import java.util.ArrayList;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 class Main
 {
     public static void main ( String [] arguments )
     {
-        ArrayList<String> firstWords = new ArrayList(Arrays.asList("find", "intersection", "two", "lists"));
-        ArrayList<String> secondWords = new ArrayList<>(Arrays.asList("find", "between", "two", "lists"));
-
-        ArrayList<String> third = new ArrayList(Arrays.asList("find", "wele", "two", "lists"));
+        ArrayList<String> third = new ArrayList(Arrays.asList("Find", "wele", "two", "lists", "FIND"));
         ArrayList<String> fourth = new ArrayList<>(Arrays.asList("find", "wele", "lists"));
 
         HashMap<ArrayList<String>, ArrayList<String>> Lists = new HashMap<>();
-        Lists.put(firstWords, secondWords);
         Lists.put(third, fourth);
 
         Main m = new Main();
-        m.TestingCommonwords(Lists);
+        System.out.println(m.getCommonWordsAdvanced(third,fourth));
+        System.out.println(m.getCommonWords(third,fourth));
     }
 
+    /*
+        Get Common Words from two list
+
+        this method doesn't exclude same words from two list
+        ex: List1 -> ["find", "common", "words", "find"], List2 -> ["find", "common"] should return ["find", "common", "find"]
+
+        this method is case-sensitive
+        ex: List1 -> ["Find"], List2 -> ["find"] should return []
+    */
     public ArrayList<String> getCommonWords(ArrayList<String> list1, ArrayList<String> list2) {
         List<String> commonWords = list1.stream().filter( list -> list2.contains(list)).collect(Collectors.toList());
         return new ArrayList<>(commonWords);
+    }
+
+
+    public ArrayList<String> getCommonWordsAdvanced(ArrayList<String> list1, ArrayList<String> list2) {
+        List<String> commonWords = list1
+                .stream()
+                .map(list -> list.toLowerCase())
+                .filter(list -> list2.stream().map(l -> l.toLowerCase()).collect(Collectors.toList()).contains(list))
+                .collect(Collectors.toList());
+        Set<String> wordSet = new LinkedHashSet<>();
+        commonWords.forEach(word -> wordSet.add(word));
+        return new ArrayList<>(wordSet);
     }
 
     public void TestingCommonwords(HashMap<ArrayList<String>, ArrayList<String>> Lists) {
@@ -31,5 +51,14 @@ class Main
             ArrayList<String> commonWords = getCommonWords(list.getKey(), list.getValue());
             System.out.println(commonWords);
         });
+    }
+
+    public ArrayList<String> generateWords(int count) {
+        Faker faker = new Faker();
+        ArrayList<String> words = new ArrayList<>();
+        IntStream.range(0, count).forEach(c -> {
+            words.add(faker.country());
+        });
+        return words;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import com.github.javafaker.Faker;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,6 +15,55 @@ public class MainTest {
         Main m = new Main();
         ArrayList<String> actualCommonWords = m.getCommonWords(firstWords, secondWords);
         Assert.assertEquals(expectedCommonWords, actualCommonWords);
+    }
+
+    @Test
+    public void ShouldReturnEmptyCommonWords() {
+        ArrayList<String> firstWords = new ArrayList(Arrays.asList("find", "intersection", "two", "lists"));
+        ArrayList<String> secondWords = new ArrayList<>(Arrays.asList("findz", "betweens", "twos", "listsz"));
+        ArrayList<String> expectedCommonWords = new ArrayList<>();
+        Main m = new Main();
+        ArrayList<String> actualCommonWords = m.getCommonWords(firstWords, secondWords);
+        Assert.assertEquals(expectedCommonWords, actualCommonWords);
+    }
+
+    @Test
+    public void ShouldReturnEmptyWhenWordsIsEmpty() {
+        ArrayList<String> firstWords = new ArrayList();
+        ArrayList<String> secondWords = new ArrayList<>(Arrays.asList("findz", "betweens", "twos", "listsz"));
+        ArrayList<String> expectedCommonWords = new ArrayList<>();
+        Main m = new Main();
+        ArrayList<String> actualCommonWords = m.getCommonWords(firstWords, secondWords);
+        Assert.assertEquals(expectedCommonWords, actualCommonWords);
+    }
+
+    @Test
+    public void ShouldNotTreatedAsCommonWordsWhenCaseSensitive() {
+        ArrayList<String> firstWords = new ArrayList(Arrays.asList("Finds"));
+        ArrayList<String> secondWords = new ArrayList<>(Arrays.asList("finds"));
+        ArrayList<String> expectedCommonWords = new ArrayList<>();
+        Main m = new Main();
+        ArrayList<String> actualCommonWords = m.getCommonWords(firstWords, secondWords);
+        Assert.assertEquals(expectedCommonWords, actualCommonWords);
+    }
+
+    @Test
+    public void ShoudlReturnTwiceWhenSameWordsInList() {
+        ArrayList<String> firstWords = new ArrayList(Arrays.asList("find", "common", "words", "find"));
+        ArrayList<String> secondWords = new ArrayList<>(Arrays.asList("find", "common"));
+        ArrayList<String> expectedCommonWords = new ArrayList<>(Arrays.asList("find", "common", "find"));
+        Main m = new Main();
+        ArrayList<String> actualCommonWords = m.getCommonWords(firstWords, secondWords);
+        Assert.assertEquals(expectedCommonWords, actualCommonWords);
+    }
+
+    @Test
+    public void TestHundredWords() {
+        Main m = new Main();
+        ArrayList<String> firstWords = m.generateWords(100000);
+        ArrayList<String> secondWords = m.generateWords(100000);
+        ArrayList<String> actualCommonWords = m.getCommonWords(firstWords, secondWords);
+        System.out.println(actualCommonWords);
     }
 }
 
